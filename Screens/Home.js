6dc947ae8b46de9df7eb86c18cf27a8a332e9d45
NasrinHaidari <nasrinhaidari786@gmail.com>
2021-03-24 11:21:24 +0430

    make FoodItems Component and call in Home Screen ane make object of foods

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View, FlatList} from 'react-native';
 import ImageSlider from 'react-native-image-slider'
 

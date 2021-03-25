8d78438cce47f4bdfb3da8efba7b8bb87211520a
NasrinHaidari <nasrinhaidari786@gmail.com>
2021-03-25 23:12:40 +0430

    make stack navigator and call Top and bottom navigator in HomeScreen page

--- App.js
+++ App.js
@@ -2,14 +2,67 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './Screens/Home'
 
+import { NavigationContainer } from '@react-navigation/native' // NavigationContainer is parent of Navigation
+import { createStackNavigator} from '@react-navigation/stack' // Top Navigation
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs' // bottom Navigation
+import { MaterialCommunityIcons } from '@expo/vector-icons' // use icon in bottom Navigation
+
+// use variable to call them in many page
+const Stack = createStackNavigator()
+const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
-    <View>
-      <Home/>
-    </View>
+    // make bottom navigator and it components and set stack and screen that use in component
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Food"
+        tabBarOptions= {{
+          activeTintColor: "#841548"
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={HomeStack}
+          options={{
+            tabBarLabel: "Home",
+            tabBarIcon: ({color, size}) => (
+              <MaterialCommunityIcons
+                name= "home"
+                color={color}
+                size={size}
+              />
+            )
+          }}
+        >
+
+        </Tab.Screen>
+      </Tab.Navigator>
+
+    </NavigationContainer>
   );
 }
+
+function HomeStack() {
+  return (
+    <Stack.Navigator
+    initialRouteName= "Home"
+    screenOptions= {{
+      headerStyle: {backgroundColor: "#841548"},
+      headerTintColor: "#fff",
+      headerTitleStyle: {fontWeight: "bold"}
+    }}
+    >
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{title: "Home Page"}}
+      />
+
+    </Stack.Navigator>
+  )
+}
+
 
 const styles = StyleSheet.create({
  

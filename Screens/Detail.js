237eeeb9c9a6cb68c4353d7914938c28dcec5525
NasrinHaidari <nasrinhaidari786@gmail.com>
2021-03-26 11:11:32 +0430

    show image, title, price, name and detail View in Detail Screen

--- Screens/Detail.js
+++ Screens/Detail.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, Button, ScrollView } from 'react-native';
-
+import { Rating } from 'react-native-ratings'
+import { useRoute } from '@react-navigation/native'
 
 export default function Detail() {
+
+    const data = useRoute().params
+    const {name, price, image, detail}= data
   return (
     <View style={styles.container}>
-
+        <Image
+        style= {{height: 190}}
+        source={{uri: image}}
+        />
+        <View>
+            <Text style={styles.title}>{name}</Text>
+            <Text style={styles.title}>Price: {price}</Text>
+            <Rating 
+                showRating 
+                imageSize={30}
+            />
+        </View>
+        <View style={{margin: 8}}>
+            <Text style={styles.title}>Introduction</Text>
+            <Text>{detail}</Text>
+        </View>
+        <Button title="Add to favorite" color="#777777"/>
     </View>
   );
 }
@@ -13,5 +33,11 @@ export default function Detail() {
 const styles = StyleSheet.create({
     container: {
         flex: 1
+    },
+    title: {
+        textAlign: "center",
+        fontWeight: "bold",
+        fontSize: 18,
+        marginTop: 8
     }
 });

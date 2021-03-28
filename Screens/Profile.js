5f55bc411f4963be0cab8cc7db7f93085196c664
NasrinHaidari <nasrinhaidari786@gmail.com>
2021-03-28 06:49:55 +0430

    import Image and use from that in Profile Screen

--- Screens/Profile.js
+++ Screens/Profile.js
@@ -4,7 +4,10 @@ import { StyleSheet, View, Image} from 'react-native';
 export default function Profile() {
   return (
     <View style={styles.container}>
-
+        <Image
+            style={{ height: 200 }}
+            source= {{uri: require("../assets/Profile.js")}}
+        />
     </View>
   );
 }

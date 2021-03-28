b5cad9a393ad696bcc5193d8460abb1ef13b4fb5
NasrinHaidari <nasrinhaidari786@gmail.com>
2021-03-28 06:36:48 +0430

    make Profile Screen

---
+++ Screens/Profile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, View, Image} from 'react-native';
+
+export default function Profile() {
+  return (
+    <View style={styles.container}>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+});
